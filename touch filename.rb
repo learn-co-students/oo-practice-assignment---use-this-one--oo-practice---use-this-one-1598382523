481fb444489b1e1b00fc9911d47eb12c0b84292a
KingFisher15 <gordond227@gmail.com>
2020-08-25 17:13:59 -0400

    Im try to use what i recall from class

--- touch filename.rb
+++ touch filename.rb
@@ -4,20 +4,21 @@ attr_accessor @name, @age
 class Patient
 
   def initialize
-    puts "Hello what is your name?"
-    name = gets.chomp
-    puts "Well hello #{@name}what is your age?"
-    age = gets.chomp
-    puts "What would you like to do?"
+     puts "Hello what is your name?"
+     name = gets.chomp
+     puts "Well hello #{@name}what is your age?"
+     age = gets.chomp
+     puts "What would you like to do?"
         puts "1. Change age"
         puts "2. Change name"
-    choice = gets.chomp
+     choice = gets.chomp
        If choice == "1"
-        puts " what is your new age?"
-        new_age = gets.chomp
-    elsif choice == "2"
-        puts "what is your new name"
-        new_name = gets.chomp
+         puts " what is your new age?"
+         new_age = gets.chomp
+            elsif choice == "2"
+              puts "what is your new name"
+              new_name = gets.chomp
+            end
        end
   end
 
