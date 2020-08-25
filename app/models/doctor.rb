dbe2370f909aa0dfd24e34332d8b618805e6e6bf
Eddy_Yunhao_Gao <as510891328@gmail.com>
2020-08-25 16:53:14 -0400

    First deliverable

---
+++ app/models/doctor.rb
@@ -0,0 +1,10 @@
+class Doctor
+  attr_accessor :name, :years
+  attr_reader :specialty
+  def initialize(name, specialty, years = 1)
+    @name = name
+    @specialty = specialty
+    @years = years
+  end
+
+end
--- run.rb
+++ run.rb
@@ -1,3 +1,9 @@
 require 'pry'
+require_relative("./app/models/patient")
+require_relative("./app/models/doctor")
 
+patient = Patient.new("Jay", 12)
+
+doctor = Doctor.new("Eddy","OCD",2)
+doctor2 = Doctor.new("John","diabety")
 binding.pry
