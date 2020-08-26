fb90808b79ee1f25a8324113525b38255cc93a5c
Eddy_Yunhao_Gao <as510891328@gmail.com>
2020-08-26 15:57:37 -0400

    Practice Deliverable

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,10 +1,24 @@
 class Doctor
   attr_accessor :name, :years
   attr_reader :specialty
+  @@all = []
   def initialize(name, specialty, years = 1)
     @name = name
     @specialty = specialty
     @years = years
+    @@all << self
+  end
+
+  def greet
+    puts "Welcome #{self.name}"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_specialty(specialty)
+    @@all.filter{ |spe| spe.specialty == specialty }
   end
 
 end
--- run.rb
+++ run.rb
@@ -6,4 +6,7 @@ patient = Patient.new("Jay", 12)
 
 doctor = Doctor.new("Eddy","OCD",2)
 doctor2 = Doctor.new("John","diabety")
+
+
+
 binding.pry
