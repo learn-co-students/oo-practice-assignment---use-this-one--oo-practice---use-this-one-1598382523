79278cf0564186189cf392f6ed56b58523de4c6b
AB <austinbuhler@gmail.com>
2020-08-26 16:06:32 -0400

    Second Commit

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,11 +3,24 @@ class Doctor
 
     attr_reader :specialty
     attr_accessor :name, :years
+    @@all = []
 
     def initialize(name, specialty, years=1)
         @name = name
         @specialty = specialty
         @years = years
+        @@all << self
     end
 
+    def greet
+        puts "Welcome, it's great to see you!"
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_specialty(specialty)  
+        self.all.filter {|doc| doc.specialty == specialty}
+    end
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,11 +1,30 @@
 
 class Patient
 
-    attr_accessor :name, :age
+    attr_accessor :name, :age, :impatience
+    @@all = []
 
     def initialize(name, age)
         @name = name
         @age = age
+        @impatience = 0
+        @@all << self
     end
+
+    def inquire_appt_ready
+        puts "The doctor will be ready soon"
+        increase_impatience
+    end
+
+    def self.all
+        @@all
+    end
+
+    private
+    
+    def increase_impatience
+        self.impatience += 1
+    end
+
 
 end
