504c2a104f737db3f13c29429beca43852202907
github username <github email address>
2020-08-26 16:27:07 -0400

    Added class and instance methods

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,11 +2,30 @@ class Doctor
 attr_accessor :name, :years
 attr_reader :specialty
 
+    @@doctors = []
+   
+
+
     def initialize(name, specialty, years)
     @name = name 
     @specialty = specialty
     @years = years
+    @@doctors << self 
+    
     end
+
+    def self.all
+        @@doctors
+    end
+
+    def greet
+        puts "Hello there!"
+    end
+
+    def self.find_by_specialty(specialty)
+        @@doctors = specialty 
+    end
+end
 
 meredith = Doctor.new("Meredith","No Specialty",9)
    
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,11 +1,34 @@
+
 class Patient 
 attr_accessor :name, :age
 
-    def initialize(string, integer)
-        @name = string
-        @age = integer
+    @@patients = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @impatience = 0
+        @@patients << self 
+
+    end
+
+    def inquire_appt_ready
+        puts "The doctor will be ready soon!"
+        increase_impatience
+    end
+
+    def self.all 
+        @@patients
+     end
+     
+
+    private 
+
+    def increase_impatience 
+        @impatience += 1
     end
 
 end
+
 
 bryam = Patient.new("Bryam",22)
