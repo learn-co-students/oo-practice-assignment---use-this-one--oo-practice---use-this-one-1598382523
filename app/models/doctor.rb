6a98ef867cee5c2ec3d1d456e67690c15615849d
jdhofmann <justindhofmann@gmail.com>
2020-08-26 16:17:51 -0400

    added in 8/27 deliverables

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,10 +3,32 @@ require 'pry'
 class Doctor
   attr_accessor :name, :years 
   attr_reader :specialty
+  @@all = []
+
   def initialize(name, specialty, years=1)
     @name = name.to_s
     @specialty = specialty.to_s
     @years = years.to_i
+    @@all << self
   end
+
+  def greet
+    return "Welcome! You'll feel better in no time"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def find_by_specialty(specialty_string)
+    specialty_array = []
+    @@all.map do | doctor|
+      if doctor.specialty == specialty_string
+        specialty_array << self
+      end
+    end
+    specialty_array
+  end
+
   binding.pry
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,9 +1,29 @@
 require 'pry'
 
 class Patient
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :impatience
+  @@all = []
+
   def initialize(name, age)
     @name = name.to_s
     @age = age.to_i
+    @impatience = 0
+    @@all << self
   end
+
+  def inquire_appt_ready
+    puts "The doctor will be ready soon"
+    increase_impatience
+  end
+
+  def self.all
+    @@all
+  end
+
+  private
+
+  def increase_impatience
+    self.impatience += 1
+  end
+  binding.pry
 end
