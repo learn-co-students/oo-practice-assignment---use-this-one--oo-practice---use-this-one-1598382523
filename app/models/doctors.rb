58e73af1c8e72363dc14ba177496c797a4e3d713
Kevin Iturralde <keviniturralde3@gmail.com>
2020-08-26 16:34:09 -0400

    little hazy

--- app/models/doctors.rb
+++ app/models/doctors.rb
@@ -1,8 +1,19 @@
 class Doctor
     attr_writer :speciality 
-    attr_accessor :name :years
+    attr_accessor :name :years :greeting
+    @all []
+
     def initialize (name, years, speciality)
         @name = name
         @years = years
         @speciality = speciality
+        @greeting = greeting
+        @@all << self
+
     end
+    def greeting 
+        puts "Welcome to our beautiful office"
+    end
+end
+
+
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,14 +1,23 @@
 require pry
 class Patient
-    attr_accessor :name :age
+    attr_accessor :name, :age 
+    attr_writer :impatient
+    @@all = []
+
     def intialize (name, age)
         @name = name
         @age = age
+        @impatient = 0
+        @@all << self
+    end
+    def inquire_appt_ready
+        puts "The doctor will be ready soon"
+        impatient.self += 1
     end
 
-
-
-steven = Patient.new ("Steven", "42")  
-
-
+Privite 
+        def increase_impatience
+            impatient.self += 1
+        end
+    end
 binding.pry
