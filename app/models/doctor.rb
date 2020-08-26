ae8af34dfdde363cb08b7393ae985beed8fdf477
zhengjianliu <comezheng@gmail.com>
2020-08-26 16:22:38 -0400

    done

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,53 +1,26 @@
 require 'pry'
 class Doctor
-    attr_reader :specialty
+    attr_reader :speciality
     attr_accessor :name, :years
-    def initialize(name,specialty,years=1)
+    @@all = []
+    def initialize(name,speciality,years=1)
       @name = name.to_s
-      @specialty = specialty.to_s
+      @speciality = speciality.to_s
       @years = years.to_i
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+    def greet
+      puts "Welcome, My name is #{@name} and how are you today?"
+    end
+
+    def self.find_by_speciality(speciality)
+      @@all.filter{ |doctor|
+        doctor.speciality == speciality
+      }
     end
 end
-
-Jay = Doctor.new("Jay","Dental")
-
-puts "Please enter one of following command:"
-puts "Do you want to see all doctor'name and their info(y/n)"
-input = gets.chomp
-
-if input == "y"
-  puts "Doctor: #{Jay.name}  | specialty: #{Jay.specialty}  | years: #{Jay.years}"
-  puts "If you want to edit the info please hit (y/n)"
-  edit = gets.chomp
-  if edit == "y"
-    puts "1. Name"
-    puts "2. specialty"
-    puts "3. Years"
-    puts "which one?"
-    choice = gets.chomp
-    if choice.to_i == 1
-      puts "Enter the new name: "
-      new_name = gets.chomp
-      Jay.name = new_name
-      puts "Doctor: #{Jay.name}  | specialty: #{Jay.specialty}  | years: #{Jay.years}"
-    elsif choice.to_i == 2
-      puts "Enter the new specialty: "
-      new_specialty = gets.chomp
-      Jay.specialty = new_specialty
-    elsif choice.to_i == 3
-      puts "Enter the new years: "
-      new_years = gets.chomp
-      Jay.years = new_years
-      puts "Doctor: #{Jay.name}  | specialty: #{Jay.specialty}  | years: #{Jay.years}"
-    else
-      puts "Bye!"
-    end
-  else
-    puts "GoodBye!"
-  end
-else
-  puts "Bye!"
-end
-
-
-#binding.pry
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,40 +1,27 @@
 class Patient
     attr_accessor :name, :age
+    @@all =[]
     def initialize(name,age)
       @name = name.to_s
       @age = age.to_i
+      @impatience = 0
+      @@all << self
     end
-end
 
-Jerry = Patient.new("Jerry","55")
-
-puts "Please enter one of following command:"
-puts "Do you want to all patient'name and their info(y/n)"
-input = gets.chomp
-
-if input == "y"
-  puts "Patient: #{Jerry.name} | Age: #{Jerry.age}"
-  puts "Edit patient\'s info? (y/n)"
-  edit = gets.chomp
-  if edit == "y"
-    puts "Which one?  1. Name | 2. Age"
-    choice = gets.chomp
-    if choice.to_i == 1
-      puts "Enter new name: "
-      new_name = gets.chomp
-      Jerry.name = new_name
-      puts "Patient: #{Jerry.name} | Age: #{Jerry.age}"
-    elsif choice.to_i == 2
-      puts "Enter new age: "
-      new_age = gets.chomp
-      Jerry.age = new_age
-      puts "Patient: #{Jerry.name} | Age: #{Jerry.age}"
-    else
-      puts "bye"
+    def self.all
+      @@all
     end
-  else
-    puts "Bye!"
-  end
-else
-  puts "Bye!!"
+
+
+    def inquire_appt_ready
+      puts "Doctor will ve ready soon!"
+      increase_impatience
+    end
+
+    private
+
+    def increase_impatience
+      self.impatience +=1
+    end
+
 end
