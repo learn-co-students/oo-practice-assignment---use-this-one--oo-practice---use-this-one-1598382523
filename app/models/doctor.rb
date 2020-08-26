b75111186e29faf6d318846d86f7c742e1804368
jakeFromm <jacobfromm@gmail.com>
2020-08-26 16:13:34 -0400

    round 2 deliverables done

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,11 +1,25 @@
 class Doctor
-    attr_accessor :name
-    attr_accessor :years
+    attr_accessor :name, :years
     attr_reader :specialty
+
+    @@all = []
     
     def initialize (name, specialty, years = 1)
         @name = name
         @specialty = specialty
         @years = years.to_i
-    end     
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    
+    def self.find_by_specialty(specialty)
+        @@all.select {|doc| doc.specialty == specialty}
+    end
+
+    def greet
+        puts "Hello there! Everything's going to be fine."
+    end
 end    
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,9 +1,26 @@
 class Patient
-	attr_accessor :name
-	attr_accessor :age
-
+	attr_accessor :name, :age, :impatience
+	@@all = []
+	
 	def initialize(name, age)
 	    @name = name
-	    @age = age.to_i
+		@age = age.to_i
+		@impatience = 0
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end 
+
+	def inquire_appt_ready
+		puts "The doctor will be ready soon."
+		increase_impatience
+	end
+
+	private
+
+	def increase_impatience
+		self.impatience += 1
 	end
 end  
