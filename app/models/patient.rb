f72c70241d7b0c18e50652924020ccc72fbfc0d9
D1yr <alexander.m.sahli@gmail.com>
2020-08-26 15:45:08 -0400

    patient deliverables set 2 done

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,10 +1,27 @@
 require 'pry'
 
 class Patient
-    attr_accessor :name, :age
-    def initialize(name, age)
+    attr_accessor :name, :age, :impatience
+    @@all = []
+    def initialize(name, age, impatience=0)
+        @impatience=impatience
         @name=name
         @age=age
+        @@all<<self
+    end
+    def self.all
+        @@all
+    end
+
+    def inquire_appt_ready
+        increase_impatience
+        puts "The doctor will be ready soon!"
+    end
+
+    private
+
+    def increase_impatience
+        self.impatience +=1
     end
 end
 
@@ -17,3 +34,7 @@ binding.pry
 #Patient#initialize: should initialize with a name (string) and age (integer) done
 #Patient#name: should return the Patient’s name and should be able to change its name after creation done
 #Patient#age: should return the Patient’s age and should be able to change its age after creation done
+#Patients should be initialized with an instance variable, impatience, set to 0 done
+#Patient#increase_impatience: should be a private method that increases impatience by 1 done
+#Patient#inquire_appt_ready: should print that the doctor will be ready soon and increase patient impatience by 1 done
+#Patient.all: should return a list of all patient instances done
