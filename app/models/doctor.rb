bd578805be57de51e9cb6bd63d8a4067bce8ed5a
johnnie71 <johnnie.agonz71@gmail.com>
2020-08-26 16:25:05 -0400

    added Patient instance variable and more, added Doc return list, greeting and specialty filter

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -4,6 +4,11 @@
 # Doctor#speciality: should return the Doctor’s speciality and should NOT be able to change its speciality after creation
 # Doctor#years: should return the Doctor’s years and should be able to change its years after creation
 
+# Doctor
+# Doctor.all: should return a list of all doctor instances
+# Doctor#greet: should print a greeting that would make any patient feel welcomed! 
+# Doctor.find_by_speciality: should take a specialty string as an argument and return a list of the doctors who have that specialty 
+
 
 
 
@@ -11,14 +16,33 @@ class Doctor
 
     attr_accessor :name, :years
     attr_reader :speciality
+
+    @@all = []
     
     def initialize(name, speciality, years=1)
 
         @name = name.to_s
         @speciality = speciality.to_s
         @years = years.to_i
+        @@all << self
 
     end
+
+    def self.all
+        return @@all
+    end
+
+    def greet
+        p "Hello, Welcome today! Get comfortable!"
+    end
+
+    def self.find_by_speciality(specialty)
+
+        doctor.find { |special| special.speciality == specialty}
+
+    end
+
+
 
 end
 
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -5,20 +5,54 @@ require 'pry'
 # Patient#name: should return the Patient’s name and should be able to change its name after creation
 # Patient#age: should return the Patient’s age and should be able to change its age after creation
 
+# Patient
+# Patients should be initialized with an instance variable, impatience, set to 0
+# Patient#increase_impatience: should be a private method that increases impatience by 1 
+# Patient#inquire_appt_ready: should print that the doctor will be ready soon and increase patient impatience by 1
+# Patient.all: should return a list of all patient instances
+
+
 class Patient
 
-    attr_accessor :name, :age
+    attr_accessor :name, :age, :impatience
+
+
+    @@all = []
     
     def initialize(name, age)
 
         @name = name.to_s
         @age = age.to_i
+        @impatience = 0
+        @@all << self
 
     end
 
+    def self.all
+        return @@all
+    end
+
+    def inquire_appt_ready
+        puts "The Doctor will be ready to see you soon."
+        self.impatience += 1
+    end
+
+
+
+ private
+
+    def increase_impatience
+        self.impatience += 1
+    end
+
+
+
 end
 
+
+
 jacob = Patient.new("Jacob", 33)
+
 
 jacob.name
 jacob.age
