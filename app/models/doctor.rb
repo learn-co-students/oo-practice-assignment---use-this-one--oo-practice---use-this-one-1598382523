a14f3997f1b5e060cb289852607bded2bfaa7df4
mahdihawary <Mahdihawary@gmail.com>
2020-08-26 15:52:18 -0400

    completed without tests

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,8 +1,20 @@
 class Doctor
+    @@all = []
+    def self.all
+        @@all
+    end
+    def greet
+        puts "Please feel welcomed patient"
+    end
+    def self.find_by_specialty(specialty)
+        self.all.select{|doctor| doctor.specialty == specialty}
+    end
+
     def initialize (name, specialty, years)
         @name= name
         @specialty = specialty
         @years = years
+        
     end
     attr_accessor :name, :years
     attr_reader :specialty
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,8 +1,19 @@
 class Patient
+    @@all= []
+    def self.all
+        @@all 
+    end
     def initialize(name, age)
         @name = name
         @age = age
+        @impatience = 0
+        @@all<<self
     end
-    attr_accessor :name, :age
+    attr_accessor :name, :age, :impatience
+    def inquire_appt_ready
+        puts "The doctor will be ready for you shortly, please be patient"
+        self.impatience +=1
+    end
+    
 end
 
