cffbc71e85fbf10de11043bb78d13d925b21a5e3
aribakhan97 <akhan41@binghamton.edu>
2020-08-26 16:30:14 -0400

    initialize patients, return doctors instances

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -9,6 +9,26 @@ class Doctor
     attr_accessor :years
  end 
 
+ doctor = []
+@@all 
+
+
+
+def greet
+puts "Hello and welcome, how can we help you today?"
+end 
+
+self.find_by_specialty(speciality)
+doctors.each {@speciality}
+end 
+
+
+
+#Doctor
+#Doctor.all: should return a list of all doctor instances
+#Doctor#greet: should print a greeting that would make any patient feel welcomed! 
+#Doctor.find_by_speciality: should take a specialty string as an argument and return a list of the doctors who have that specialty 
+
  #attr_accessor -> get && set
  #attr_reader -> get 
  #attr_writer -> set 
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,8 +3,28 @@
     def initialize(name, age)
         @name = name 
         @age = age 
+        @impatience = 0 
+    
     end 
+  
     attr_accessor :name
     attr_accessor :age 
+
+    Private
+    
+patients = []
+    def increase_impatience 
+        @impatience += 1
+    end 
+
+    def inquire_appt_ready
+        puts " The doctor will be ready soon "
+        @impatience += 1 
+    end 
+
+    def patients @@all
+        return patients 
+    end  
+
  end 
 
