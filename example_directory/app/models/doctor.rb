19c6a33f01a79c33cec0266700703ede2b4fa03f
gerardmi24 <gerardmistretta@yahoo.com>
2020-08-27 16:13:29 -0400

    Really struggled with thiss practice - a lot of incomplete methods

--- example_directory/app/models/doctor.rb
+++ example_directory/app/models/doctor.rb
@@ -19,6 +19,29 @@ class Doctor
     end
     
     def self.find_by_speciality(speciality)
-        
+        #code here
+        self.all.filter do |doctor|
+            doctor.specialty = specialty
+        end
+    end
+
+    def patients
+        #array of all current Patients for each doctor
+        all_patients=[]
+        all_patients << self.Patients.new
+    end
+
+    def discharge_patient
+        #set doctor to nil if patient belonged to current doctor
+            self.patient.each do |patient|
+              
+              patient.owner = nil
+            end
+    end
+
+    def transfer_patient
+        #transfer patient to another doctor if patient belongs to current doctor
+        if patient
+
     end
 end
--- example_directory/app/models/patient.rb
+++ example_directory/app/models/patient.rb
@@ -17,9 +17,20 @@ class Patient
         puts "The doctor will be ready soon."
         increase_impatience
     end
+
+    def doctor()
+        doctor
+    end
     
+    def change_doctors(doctor)
+        #update the patient's doctor
+    end
+
+    patient = Doctor.new
+
     private
     def increase_impatience
         self.impatience += 1
     end
+
 end
